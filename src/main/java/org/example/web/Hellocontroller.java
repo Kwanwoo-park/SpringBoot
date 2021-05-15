e530a418766bb671407dea5450e17e9ce82af0a9
Kwanwoo-park <65990897+Kwanwoo-park@users.noreply.github.com>
2021-05-15 20:02:25 +0900

    second commit

---
+++ src/main/java/org/example/web/Hellocontroller.java
@@ -0,0 +1,12 @@
+package org.example.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hellocontroller {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
